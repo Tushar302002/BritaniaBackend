0e23cb0396d12f17e179ee828b0eb21ce9198044
sanjeevKalvig <sanjeev.kumar@kalvig.in>
2025-12-18 14:02:53 +0530

    ar2 added

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
+app.use("/uploads", express.static("uploads"));
+
 /* =========================
    DATABASE
 ========================= */
